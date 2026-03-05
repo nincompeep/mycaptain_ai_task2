7353b6470e2d23e9b21eb5b3fb989d8784f66d44
nincompeep <pranavsahire69@gmail.com>
2026-03-05 12:29:42 +0530

    Add files via upload

---
+++ coodingloops.py
@@ -0,0 +1,18 @@
+list1 = [12, -7, 5, 64, -14]
+
+print("Input:", list1)
+print("Output:", end=" ")
+
+for num in list1:
+    if num > 0:
+        print(num, end=", ")
+print("\n")
+
+list2 = [12, 14, -95, 3]
+
+print("Input:", list2)
+print("Output:", end=" ")
+
+for num in list2:
+    if num > 0:
+        print(num, end=", ")
